630d65733636131db7ab0d6c652470708154744e
WeronikaRadtke <weronikaradtke14@gmail.com>
2023-08-03 15:43:25 +0200

    navbar_cz2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import "@fontsource/open-sans"; // Defaults to weight 400
 import ustawieniaicon from '../../logo/ustawieniaicon.png';
 
 
 const Navbar = () => {
+  const [showHTPandSET, setShowHTPandSET] = useState(false); 
+
+  const handleClick = () => {
+    setShowHTPandSET(!showHTPandSET);
+  };
+
   return (
     <div className="navbar-container">
       <div className="left-container">
@@ -13,9 +19,15 @@ const Navbar = () => {
       <div className="center-container">
         {/* Treść centralnego kontenera */}
       </div>
-      <div className="right-container">
+      <div className="right-container" onClick={handleClick}>
         <div className="settings-text"> Ustawienia</div>
         <img src={ustawieniaicon} alt="UstawieniaIcon" width="30" height="30" />
+        {showHTPandSET && (
+          <div className="HTPandSET-container">
+            <div className="smallHTP">Jak grać?</div>
+            <div className="smallSET">Ustawienia</div>
+            </div>
+        )}
       </div>
     </div>
   );
